aebad92f1278189ca713f2ff7216a9e05a1a87c4
Ranya <laasriranya1@gmail.com>
2024-03-27 18:05:26 +0000

    design DataBase Schema for Properties #12

--- api/controllers/user.Controllers.js
+++ api/controllers/user.Controllers.js
@@ -1,4 +1,19 @@
 const {DeleteUserDB} = require('../models/methods/user.Methods')
+const {getAllUsersDB}= require('../models/methods/user.Methods')
+
+
+
+exports.getAllUsers = async (req,res) => {
+  try {
+    const users = await getAllUsersDB();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error)
+
+  }
+};
+
+
 
 exports.DeleteUser = async (req, res) => {
   try {
