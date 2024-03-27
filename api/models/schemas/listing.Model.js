a4feacf5eede83e8fc3c214e1f9b577e449b63ff
Ranya <laasriranya1@gmail.com>
2024-03-27 19:15:29 +0000

    design DataBase Schema for Properties #12

---
+++ api/models/schemas/listing.Model.js
@@ -0,0 +1,56 @@
+const mongoose = require ('mongoose')
+const Schema = mongoose.Schema
+
+
+const ListingsSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    listingType: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    size: {
+      type: Number
+    },
+    reviews: [
+      {
+        userId: {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+        },
+        rating: {
+          type: Number,
+          
+        },
+        comment: {
+          type: String
+        }
+      }
+    ],
+    images: [String],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  });
+  
+  module.exports = mongoose.model('Linstings', ListingsSchema)
