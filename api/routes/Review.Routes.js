59768e393f2ba3f5e044e4f24b72b349106b5eee
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-03-30 08:53:12 +0000

    Added Routes for Reviews

---
+++ api/routes/Review.Routes.js
@@ -0,0 +1,14 @@
+const {CreateReview} = require('../controllers/reviews.Controllers')
+const express = require('express')
+const ReviewRoute = express.Router()
+const {isAuthenticated} = require('../middlewares/authMiddlewares')
+
+
+
+
+
+ReviewRoute.post('/',isAuthenticated,CreateReview)
+
+
+
+module.exports = ReviewRoute
