037316b2daccfbb8e418de358c1544ed2ae43140
Ranya <laasriranya1@gmail.com>
2024-03-31 17:05:11 +0000

    Implement uuid for users #56

--- api/routes/auth.Routes.js
+++ api/routes/auth.Routes.js
@@ -1,6 +1,6 @@
 //import midllewares and packs
 const express = require('express');
-const { getRegister, getLogin, ResetPassword ,  GenrateTempToken} = require('../controllers/auth.Controllers');
+const { getRegister, getLogin, ResetPassword ,  GenrateTempToken, logout} = require('../controllers/auth.Controllers');
 const AuthRoute = express.Router();
 const { AvoidAuth } = require('../middlewares/avoidAuth')
 
@@ -11,6 +11,7 @@ AuthRoute.post('/register', AvoidAuth, getRegister)
 AuthRoute.get('/login', AvoidAuth, getLogin)
 AuthRoute.get('/reset-password', GenrateTempToken)
 AuthRoute.post('/reset-password/:id/:token', ResetPassword)
+AuthRoute.post('/logout', logout)
 
 //export route
 module.exports = AuthRoute
