e362c56af2036df75a1b15203676f1e7de53af4f
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-03-31 10:21:27 +0000

    Added Route to Reset password resolves issue #53

--- api/routes/auth.Routes.js
+++ api/routes/auth.Routes.js
@@ -1,14 +1,16 @@
 //import midllewares and packs
 const express = require('express');
-const { getRegister, getLogin } = require('../controllers/auth.Controllers');
+const { getRegister, getLogin, ResetPassword ,  GenrateTempToken} = require('../controllers/auth.Controllers');
 const AuthRoute = express.Router();
 const { AvoidAuth } = require('../middlewares/avoidAuth')
 
 
 //Authentication
 
-AuthRoute.post('/auth/register', AvoidAuth, getRegister)
-AuthRoute.get('/auth/login', AvoidAuth, getLogin)
+AuthRoute.post('/register', AvoidAuth, getRegister)
+AuthRoute.get('/login', AvoidAuth, getLogin)
+AuthRoute.get('/reset-password', GenrateTempToken)
+AuthRoute.post('/reset-password/:id/:token', ResetPassword)
 
 //export route
 module.exports = AuthRoute
