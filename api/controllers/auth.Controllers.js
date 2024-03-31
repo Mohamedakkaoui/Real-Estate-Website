bd179d2d07ef953f5b1e284c4f75871079d6aca8
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-03-31 10:17:38 +0000

    Implemented Functions to reset a User password resolves issue #53

--- api/controllers/auth.Controllers.js
+++ api/controllers/auth.Controllers.js
@@ -2,8 +2,8 @@
 const UserSchema = require('../models/schemas/user.Model.js');
 const { mailsender } = require('../middlewares/nodemailer.js')
 const { HashPassword, VerifyPassword } = require('../helpers/hashing.js');
-const { generateToken } = require('../helpers/jwt');
-const { checkExitingMail } = require('../models/methods/user.Methods.js')
+const { generateToken , verifyToken } = require('../helpers/jwt');
+const { GetUserbyIdDB, updateProfileDB , checkExitingMail } = require('../models/methods/user.Methods.js')
 
 
 
@@ -18,7 +18,7 @@ exports.getRegister = async (req, res) => {
         }
         const newUser = new UserSchema({ FirstName, LastName, Username, Email, Password: hashedPassword, PhoneNumber })
         const result = await newUser.save()
-        mailsender(req.body.Email, LastName)
+        // mailsender(req.body.Email,'welcomingEmail', LastName)
         return res.status(201).send({ message: 'signing up successfully', result: result })
     } catch (err) {
         return res.status(404).send('Unable to Register user : ' + err)
@@ -33,14 +33,59 @@ exports.getLogin = async (req, res) => {
         const { Email, Password } = req.body
         const user = await UserSchema.findOne({ Email })
         if (!user) {
-            return res.status(400).json({ message: "User not founds" })
+            return res.status(400).json({ message: "User not found" })
         }
         const checked = await VerifyPassword(Password, user.Password)
         if (!checked) return res.status(400).json({ message: "Incorrect Password" })
-        const token = generateToken({ Email: user.Email, FirstName: user.FirstName, LastName: user.LastName, id: user._id })
+        const token = generateToken({ Email: user.Email, FirstName: user.FirstName, LastName: user.LastName, id: user._id }, '10h')
         res.status(200).json({ message: "Login successful", token: token });
 
     } catch (err) {
         return res.status(501).send('Unsuccessful attempt to login :' + err)
+    }
+}
+
+
+//Generate temporary token
+exports.GenrateTempToken = async (req, res) => {
+    try {
+        const { Email } = req.body
+        const user = await checkExitingMail(Email)
+        if (!user) {
+           return res.status(400).json({ message: "User not found" })
+        }
+        const id = user._id
+        const token = generateToken({ id , Username: user.Username }, '15m')
+        mailsender(req.body.Email,'resetPasswordEmail', id , token)
+        return res.status(200).send('Password reset Link sent to ur Email')
+    } catch (error) {
+      return res.status(500).json({ message : 'Internal server error', error : error.message})
+    }
+}
+
+
+
+
+//Reset password
+exports.ResetPassword = async (req, res) => {
+    try {
+        const { id , token } = req.params
+        const user = await GetUserbyIdDB(id)
+        if (!user) {
+           return res.status(400).json({ message: "User not found" })
+        }
+        const verify = await verifyToken(token)
+        if (!verify) {
+            return res.status(404).send('token ivalid')
+        }
+        const { password , ConfirmPassword} = req.body
+        if (password !== ConfirmPassword) {
+            return res.status(400).json({message : 'Password is not Matched'})
+        }
+        const hashedpassword = await HashPassword(password)
+        await updateProfileDB (id , {Password : hashedpassword}, {new : true})
+        return res.status(202).json({message : 'password reset successfuly!'})
+    } catch (error) {
+        return res.status(500).json({message : 'Unable to reset password due to :' , Error : error.message} )
     }
 }
