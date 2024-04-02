7ec1893cd3117a40cbcc76548842f9499b978e25
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-04-02 12:22:51 +0000

    refactored

--- api/routes/listing.Routes.js
+++ api/routes/listing.Routes.js
@@ -19,7 +19,7 @@ const ImagesInfo = require('../config/constants')
 //defining routes
 ListingRoute.get('/',getListings)
 ListingRoute.get('/:id',getListingById)
-ListingRoute.post('/add', ListingPicsUpload.array('image', ImagesInfo.Max_file_to_Upload), isAuthenticated, addNewListing)
+ListingRoute.post('/add', ListingPicsUpload.array('images', ImagesInfo.Max_file_to_Upload), isAuthenticated, addNewListing)
 ListingRoute.delete('/:id', isAuthenticated, deleteListing)
 
 
