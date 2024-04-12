f4022e1ec59e83ad0b66f8c74357c2a6abec680b
Ranya <laasriranya1@gmail.com>
2024-04-12 23:00:32 +0000

    add verify endpoint to verify the verification code #60

--- api/routes/auth.Routes.js
+++ api/routes/auth.Routes.js
@@ -3,11 +3,13 @@ const express = require('express');
 const { getRegister, getLogin, ResetPassword ,  GenrateTempToken, logout} = require('../controllers/auth.Controllers');
 const AuthRoute = express.Router();
 const { AvoidAuth } = require('../middlewares/avoidAuth')
-const {validateUser} = require('../middlewares/validate/validateUser')
+const {validateUser} = require('../middlewares/validate/validateUser');
+const {verifyVerificationCode } = require('../middlewares/VerifNumber');
 
 //Authentication
 
 AuthRoute.post('/register', AvoidAuth,validateUser,getRegister)
+AuthRoute.post('/verify', verifyVerificationCode);
 AuthRoute.get('/login', AvoidAuth, getLogin)
 AuthRoute.get('/reset-password', GenrateTempToken)
 AuthRoute.post('/reset-password/:id/:token', ResetPassword)
