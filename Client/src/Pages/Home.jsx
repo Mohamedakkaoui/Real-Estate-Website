323a88e9c6153f4dff193933fe6ba01bb2a00e63
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-13 18:07:34 +0100

    Added pages for authentification

---
+++ Client/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../Components/Forum/Footer";
+import NavBar from "../Components/Forum/NavBar";
+import Toast from "../Components/Common/Toast/Toast";
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
