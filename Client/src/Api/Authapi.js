bc151e97743be9ea6ebf1997b4e82aefbd59d53a
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-13 17:57:01 +0100

    feat: Implement user authentication API endpoints

---
+++ Client/src/Api/Authapi.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3500",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+export function LoginUser(body) {
+  return api.post("users/auth/login", body);
+}
+
+export function RegisterUser(body) {
+  return api.post("users/auth/register", body);
+}
+
+export function VerifyEmail (email, token) {
+  return api.get(`users/auth/verify?email=${email}&token=${token}`)
+}
+
+export function ResetPassword (body) {
+  console.log("entered");
+  console.log(body);
+  return api.post("users/auth/reset-password-email", body)
+}
+
+
+export function UpdatePassword (id, token ,body) {
+  return api.post(`users/auth/password-reset/${id}/${token}`, body)
+}
+
+
+export default api
