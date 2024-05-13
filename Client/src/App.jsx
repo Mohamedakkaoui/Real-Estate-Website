60854a9512724b437bf7088de0e05b8f7bf90b2c
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-13 18:08:03 +0100

    Modifications

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,34 @@
+import React, { useState } from "react";
+import Home from "./Pages/Home";
+import CheckEmail from "./Pages/CheckEmail";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./Pages/NotFound";
+import ResetPassword from "./Pages/ResetPassword";
+import UpdatePassword from "./Pages/UpdatePssword";
+import { AuthProvider } from "./Context/AuthContext";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/users/auth/verify" element={<CheckEmail />} />
+            <Route
+              path="/users/auth/reset-password-email"
+              element={<ResetPassword />}
+            ></Route>
+            <Route
+              path="/users/auth/password-reset"
+              element={<UpdatePassword />}
+            ></Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ Client/tailwind.config.js
@@ -0,0 +1,17 @@
+const flowbite = require("flowbite-react/tailwind");
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [flowbite.plugin(), nextui()],
+};
