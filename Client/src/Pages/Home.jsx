fdb73628dd6ad95b4ae7af8f688ee6b7c640f610
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-15 23:56:22 +0100

    Implement context for user data and conditional rendering in Home component

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../Components/Forum/Footer";
 import NavBar from "../Components/Forum/NavBar";
-import Toast from "../Components/Common/Toast/Toast";
+import { ContextAuth } from "../Context/AuthContext";
 
 function Home() {
+
+  
+  const { isLoading, UserProfile } = ContextAuth();
+
+
   return (
     <>
       <NavBar />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {UserProfile && UserProfile.Email ? (
+            <div>Welcome, {UserProfile.Email}!</div>
+          ) : (
+            <div>You are not logged in.</div>
+          )}
+        </div>
+      )}
       <Footer />
     </>
   );
