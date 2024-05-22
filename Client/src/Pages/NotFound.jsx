982c8c86af15b633f88a55c183cb72cdab8da4ef
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-22 12:56:03 +0100

    imporved the notfound page

--- Client/src/Pages/NotFound.jsx
+++ Client/src/Pages/NotFound.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import NotFoundImg from "../assets/404.png"
 function NotFound() {
+
+  const handleGoBack = () => {
+    window.history.back();
+  };
+
   return (
-    <div>
-      <h1>404 Page not Found</h1>
+    <div className="flex justify-center flex-col min-h-screen text-center ">
+      <img src={NotFoundImg} alt="" className="w-[500px] mx-auto" />
+      <h1 className="text-6xl font-bold opacity-50">Page Not Found!!</h1>
+      <button
+        className="gap-2 mt-8 btn btn-primary rounded-md text-white flex-center-center"
+        onClick={handleGoBack}
+      >
+        <span className='bg-[#FFA920] px-5 py-2 rounded-md hover:cursor-pointer'>go back</span>
+      </button>
     </div>
   )
 }
