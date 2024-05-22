d57067db77ef6ae4b39ec5e54162359eac09fb69
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-22 12:55:17 +0100

    modified and improved the pages

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -2,28 +2,27 @@ import React, { useEffect, useState } from "react";
 import Footer from "../Components/Forum/Footer";
 import NavBar from "../Components/Forum/NavBar";
 import { ContextAuth } from "../Context/AuthContext";
-
+import Hero from "../Components/Home/Hero";
+import AboutUs from "../Components/Home/AboutUs";
+import Featured from "../Components/Home/Featured";
+import Categories from "../Components/Home/Categories";
+import Cities from "../Components/Home/Cities";
+import SellingOption from "../Components/Home/SellingOption";
+import ScrollToTopButton from "../Components/Common/ScrollToTopButton";
 function Home() {
-
-  
   const { isLoading, UserProfile } = ContextAuth();
-
 
   return (
     <>
-      <NavBar />
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          {UserProfile && UserProfile.Email ? (
-            <div>Welcome, {UserProfile.Email}!</div>
-          ) : (
-            <div>You are not logged in.</div>
-          )}
-        </div>
-      )}
-      <Footer />
+      <NavBar/>
+      <Hero/>
+      <AboutUs/>
+      <Featured/>
+      <Categories/>
+      <Cities/>
+      <ScrollToTopButton/>
+      <SellingOption/>
+      <Footer/>
     </>
   );
 }
