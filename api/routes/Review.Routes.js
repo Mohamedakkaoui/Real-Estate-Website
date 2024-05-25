a1f2b47fd1934b791b8464607aead1a3f8ca9bbe
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-25 18:26:42 +0100

    added an endpoint for users'Listings reviews

--- api/routes/Review.Routes.js
+++ api/routes/Review.Routes.js
@@ -1,5 +1,5 @@
 
-const {CreateReview , GetReviewById , updateReview , deleteReview, getAllReviews,getReviewUserAddTop} = require('../controllers/reviews.Controllers')
+const {CreateReview , GetReviewById , updateReview , deleteReview, getAllReviews,getReviewUserAddTop, GetMyListingsReviews} = require('../controllers/reviews.Controllers')
 const express = require('express')
 const ReviewRoute = express.Router()
 const { isAuthenticated } = require('../middlewares/authMiddlewares')
@@ -16,7 +16,7 @@ const verifyRoles = require('../middlewares/roles.js')
 ReviewRoute.get('/', getAllReviews)
 // ReviewRoute.put('/:id', isAuthenticated, verifyRoles(ROLES_LIST.User), IsOwner, updateReview)
 
-
+ReviewRoute.get('/MylistingReviews',isAuthenticated, GetMyListingsReviews)
 // ReviewRoute.delete('/:id', isAuthenticated, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.User), IsOwner, deleteReview)
 ReviewRoute.get('/:userID',isAuthenticated,getReviewUserAddTop)
 
