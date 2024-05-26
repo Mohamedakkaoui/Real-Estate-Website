7d4665d9a029166602d41eb624cc9ca9b29ba33c
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-26 18:22:47 +0100

    refactored to display the nearby properties

--- Client/src/Components/Home/Featured.jsx
+++ Client/src/Components/Home/Featured.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import SingleProductCard from "../Common/Card/SingleCard";
-import { property }  from "../../Data/DummyData"
+import ListingsNeraby from '../../Hooks/ListingsNeraby';
+import { GetAllListings } from '../../Api/ListingsApi';
+import Loading from '../Common/Loading';
 
 
 function Featured() {
+
+  const { city, listings, loading, error} = ListingsNeraby()
+  if (!listings.length > 0) {
+    const getalllsitings = async () => {
+      const res  = await GetAllListings()
+      const listings = res.data.listings
+    }
+  }
+  
+  //another condiiton if the arrya legth is 0
+  
   return (
 
     <div className="pt-10 pb-16 w-[90%]  mx-auto mt-10">
       <div className="text-center">
         <h1 className="mx-auto sub-heading">featured</h1>
-        <h1 className="heading">explore featured latest properties</h1>
+        <h1 className="heading">explore featured nearby properties</h1>
       </div>
       <div className="flex flex-wrap gap-4 mt-8">
-        {property.slice(0, 3).map((featured) => (
-          <SingleProductCard key={featured.id} {...featured} />
-        ))}
+        {!loading ? listings.slice(0, 3).map((featured) => (
+          <SingleProductCard key={featured._id} {...featured} />
+        )) : <div className="flex items-center justify-center h-screen"><Loading  /></div>}
       </div>
     </div>
   );
