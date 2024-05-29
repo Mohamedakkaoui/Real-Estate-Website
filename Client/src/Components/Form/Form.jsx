86315244950a9481afbc8ee1cf059788457a40c2
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-29 18:54:31 +0100

    changed the form component task :  update user prfil/pasowrd/profile pic

--- Client/src/Components/Form/Form.jsx
+++ Client/src/Components/Form/Form.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import { AddBio, PersonalInfos, ProfilePic } from './FormComps'
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
-import './Form.css'
+import React from "react";
+import { PersonalInfos, ProfilePic, AddBio } from "./FormComps";
+import "./Form.css";
 
 function Form() {
-    return (
-        <div className='flex flex-wrap justify-center'>
-            <div className='w-[50%] px-4'> <PersonalInfos /></div>
-            <div className='flex flex-col'>
-                <ProfilePic />
-                <AddBio />
-            </div>
-
-        </div >
-    )
+  return (
+    <div className="flex flex-wrap justify-center">
+      <div className="w-[50%] px-4 mr-3">
+        <PersonalInfos />
+        <button className="middle none center w-full rounded-lg bg-red-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-orange-500/20 transition-all hover:shadow-lg hover:shadow-orange-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+          Delete Account
+        </button>
+      </div>
+      <div className="flex flex-col">
+        <ProfilePic />
+        <AddBio />
+      </div>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
