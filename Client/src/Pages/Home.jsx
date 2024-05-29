502fffbea95874ba3c99d738f4dd612dbadf7277
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-05-29 18:56:39 +0100

    cleaned the file

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Footer from "../Components/Forum/Footer";
 import NavBar from "../Components/Forum/NavBar";
-import { ContextAuth } from "../Context/AuthContext";
 import Hero from "../Components/Home/Hero";
 import AboutUs from "../Components/Home/AboutUs";
 import Featured from "../Components/Home/Featured";
@@ -10,7 +9,6 @@ import Cities from "../Components/Home/Cities";
 import SellingOption from "../Components/Home/SellingOption";
 import ScrollToTopButton from "../Components/Common/ScrollToTopButton";
 function Home() {
-  const { isLoading, UserProfile } = ContextAuth();
 
   return (
     <>
