c4bb7354b978a4be3410edd380092e13ed50c2df
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-06-03 22:03:51 +0100

    website review methods

---
+++ api/models/methods/SiteReviewsMethods.js
@@ -0,0 +1,31 @@
+const SiteReviewSchema = require("../schemas/SiteReviews.Model");
+
+//add a review
+exports.AddSiteReviewDB = async (data) => {
+  try {
+    const Review = new SiteReviewSchema(data)
+    return await Review.save()
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+// Delete review by ID
+exports.deleteSiteReviewDB = async (id) => {
+  try {
+    const deletedReview = await SiteReviewSchema.deleteOne({ Object_id: id });
+    return deletedReview;
+  } catch (err) {
+    throw new Error("Failed to delete review: " + err.message);
+  }
+}
+
+// get all reviews
+exports.getAllSiteReviewsDB = async () => {
+  try {
+    const Reviews = await SiteReviewSchema.find();
+    return Reviews;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
