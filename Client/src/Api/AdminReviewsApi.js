cf6669ad34f9afc1d747c1462c89d3d7dd987dec
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-06-03 22:10:56 +0100

    api routing for the admin dashboard

---
+++ Client/src/Api/AdminReviewsApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3500/reviews",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+})
+
+export function getAllSiteReviews() {
+  return api.get("/admin/Reviews");
+}
+
+export function AddSiteReview(body) {
+  return api.post('/admin/Reviews', body)
+}
+
+export function DeleteSiteAdmin(id) {
+  return api.delete(`/admin/Reviews/${id}`)
+}
+
+export default api
