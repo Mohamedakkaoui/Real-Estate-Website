56a383e88ccfd1c188c7315a5acccb75897cfcfb
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-06-27 15:26:07 +0100

    Refactored handled te saving button in property detail

--- Client/src/Components/PropertyDetail/saveListing.jsx
+++ Client/src/Components/PropertyDetail/saveListing.jsx
@@ -1,34 +1,80 @@
-import React, { useState } from 'react';
-import { Heart } from 'lucide-react';
-import { saveListing } from '../../Api/ListingsApi';
+import React, { useState, useEffect } from "react";
+import { DeleteSaved, saveListing } from "../../Api/ListingsApi";
+import { ContextAuth } from "../../Context/AuthContext";
+import { FaHeart } from "react-icons/fa6";
+import { Toaster, toast } from "sonner";
 
 const SaveListingComponent = ({ id, onSave }) => {
+  const { UserProfile } = ContextAuth();
   const [isSaved, setIsSaved] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const checkSavedStatus = () => {
+      if (
+        UserProfile &&
+        UserProfile.watchList &&
+        UserProfile.watchList.includes(id)
+      ) {
+        setIsSaved(true);
+      }
+    };
+    checkSavedStatus();
+  }, [UserProfile, id]);
 
   const handleSave = async () => {
     setIsLoading(true);
     try {
-      console.log('Saving listing ID:', id);
-      const response = await saveListing(id);
-      console.log('Listing saved:', response);
-      setIsSaved(true);
+      if (isSaved) {
+        const response = await DeleteSaved(id);
+        if (response.status == 200) {
+          toast.success(response.data.Message, {
+            style: { backgroundColor: "#76C776", color: "white" },
+          })
+          setIsSaved(false);
+        } else {
+          toast.error("Error occured while unsaving Property", {
+            style: { backgroundColor: "#FF7F7F", color: "white" },
+          })
+          console.log(response)
+        }     
+      } else {
+        const response = await saveListing(id);
+        console.log(response)
+        if (response.message == "Listing saved successfully") {
+          toast.success(response.message, {
+            style: { backgroundColor: "#76C776", color: "white" },
+          })
+          setIsSaved(true);
+        } else {
+          toast.error("Error occured while saving Property", {
+            style: { backgroundColor: "#FF7F7F", color: "white" },
+          })
+        }
+      }
       if (onSave) {
-        onSave(); // Refresh favorites if onSave callback is provided
+        onSave();
       }
     } catch (error) {
-      console.error('Error saving listing:', error);
+      console.error("Error saving listing:", error);
     } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <div onClick={handleSave} className={`flex gap-2 border rounded-lg px-2 pt-2 ${isSaved ? 'text-red-600' : 'text-grey-600'} hover:cursor-pointer`}>
-      <span>{isSaved ? 'saved' : 'save'}</span>
-      <Heart className="size-5" />
-      {isLoading && <span>Loading...</span>}
-    </div>
+    <>
+      <div
+        onClick={handleSave}
+        className={`flex gap-2 border rounded-lg px-2 pt-2 ${
+          isSaved ? "text-red-600" : "text-grey-600"
+        } hover:cursor-pointer`}
+      >
+        <span>{isSaved ? "Saved" : "Save"}</span>
+        <FaHeart className="size-5 w-6" />
+        {isLoading && <span>Loading...</span>}
+      </div>
+      <Toaster position="top-center" />
+    </>
   );
 };
 
